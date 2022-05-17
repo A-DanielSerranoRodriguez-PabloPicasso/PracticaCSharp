0ddab50fa51f4f2f9a5e91fc172a121c39dfaf24
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablopicasso.es>
2022-05-17 14:21:11 +0200

    Añadido el registro de usuarios

--- Pagina/Archivos/js/register.js
+++ Pagina/Archivos/js/register.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-    $("#btn-login").click(function () {
-        let username = $("#email").val().trim();
+    $("#btn-register").click(function () {
+        let username = $("#userName").val().trim();
         let password = $("#pwd").val().trim();
+        let email = $("#email").val().trim();
+        let birthDate = $("#birthDate").val().trim();
 
         console.log(username)
 
         if (username != "" && password != "") {
             $.ajax({
-                url: 'https://localhost:7179/User/login',
+                url: 'https://localhost:7179/User/register',
                 type: 'post',
-                data: JSON.stringify({ UserName: username, Password: password }),
+                data: JSON.stringify({ UserName: username, Password: password, Email: email, BirthDate: birthDate }),
                 contentType: "application/json; charset=utf-8",
                 success: function (response) {
                     if (response) {
-                        window.location = "views/welcome.html";
-                    } else {
-                        $("#error").fadeIn();
+                        window.location = "../index.html";
                     }
                 },
                 error: function () { $("#error").fadeIn() }
